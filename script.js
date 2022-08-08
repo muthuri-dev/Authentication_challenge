bb0659f435edfba7c368d4f8d7842e6fea00728c
kennedy muthuri <muthurikennedy082@gmail.com>
2022-08-08 08:02:49 +0300

    added js code

--- script.js
+++ script.js
@@ -1,18 +1,59 @@
 
-var username=document.querySelector('#username').value;
-var email=document.querySelector('#email').value;
-var password=document.querySelector('#password').value;
+const username=document.getElementById('username').value;
+const email=document.getElementById('email').value;
+const password=document.getElementById('password').value;
+const output=document.querySelector('.output');
 
-var user={
-    username:username,
-    email:email,
-    password:password
+//signup function
+function signup(event){
+    event.preventDefault();
+
+    var user={
+        username:username,
+        email:email,
+        password:password
+    }
+   localStorage.setItem('user',JSON.stringify(user));
+   console.log(localStorage.getItem('user'));
+   displayData();
 }
-var userSerialized=JSON.stringify(user);
+
+function displayData(){
+    let username,email,password=JSON.parse(localStorage.getItem('user'));
+    output.innerHTML=`
+    <ul>
+        <li>${username}</li>
+    </ul>
+    `
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ var userSerialized=JSON.stringify(user);
+    localStorage.setItem('user',userSerialized);
+    alert('saved');
 console.log(userSerialized);
 
-localStorage.setItem('user',userSerialized);
+
 
 var userDeserialized=JSON.parse(localStorage.getItem('user'))
 
 console.log(userDeserialized);
+
+btn.addEventListener('click',function(e){
+    e.preventDefault();
+})*/
