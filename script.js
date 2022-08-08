1fef1a7e628722b4dc6247659216a0f3b281997a
kennedy muthuri <muthurikennedy082@gmail.com>
2022-08-08 10:52:47 +0300

    added some js

--- script.js
+++ script.js
@@ -1,17 +1,61 @@
 
-const username=document.getElementById('username').value;
-const email=document.getElementById('email').value;
-const password=document.getElementById('password').value;
+const username=document.getElementById('username');
+const email=   document.getElementById('email');
+const password=document.getElementById('password');
+
+
+//submitting data to the local storage
+const btn=document.querySelector('.btn');
+btn.addEventListener('click',function(event){
+    event.preventDefault();
+    var user={
+        username:username.value,
+        email:email.value,
+        password:password.value
+    }
+    console.log(user);
+    localStorage.setItem('user',JSON.stringify(user));
+    console.log(localStorage.getItem('user'));
+
+    //notifying the user for saved data
+    var result=JSON.parse(localStorage.getItem('user'));
+    window.alert('Data saved');
+    console.log(result);
+
+    //redirecting the user to another page/route after registration
+    window.location.href='./services/login.html';
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //signup function
-function signup(event){
+/*function signup(event){
     event.preventDefault();
 
     var user={
-        username:username,
-        email:email,
-        password:password
+        username:username.value,
+        email:email.value,
+        password:password.value
     }
    window.localStorage.setItem('user',JSON.stringify(user));
+  
 }
-
+*/
