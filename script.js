dd8b7b3af5ef58de01e57f170d7209eb04ffe30a
kennedy muthuri <muthurikennedy082@gmail.com>
2022-08-08 09:05:27 +0300

    dummy code

--- script.js
+++ script.js
@@ -2,7 +2,6 @@
 const username=document.getElementById('username').value;
 const email=document.getElementById('email').value;
 const password=document.getElementById('password').value;
-const output=document.querySelector('.output');
 
 //signup function
 function signup(event){
@@ -13,47 +12,6 @@ function signup(event){
         email:email,
         password:password
     }
-   localStorage.setItem('user',JSON.stringify(user));
-   console.log(localStorage.getItem('user'));
-   displayData();
+   window.localStorage.setItem('user',JSON.stringify(user));
 }
 
-function displayData(){
-    let username,email,password=JSON.parse(localStorage.getItem('user'));
-    output.innerHTML=`
-    <ul>
-        <li>${username}</li>
-    </ul>
-    `
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
- var userSerialized=JSON.stringify(user);
-    localStorage.setItem('user',userSerialized);
-    alert('saved');
-console.log(userSerialized);
-
-
-
-var userDeserialized=JSON.parse(localStorage.getItem('user'))
-
-console.log(userDeserialized);
-
-btn.addEventListener('click',function(e){
-    e.preventDefault();
-})*/
